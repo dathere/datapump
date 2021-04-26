b4b1d571e149f99f3945eb99a105437cd1715016
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2021-04-26 18:39:10 -0400

    Add logging; configfile

--- datapump.py
+++ datapump.py
@@ -1,21 +1,41 @@
 import logging
+from colorama import Fore, Back, Style
 import click
+import click_config_file
 from ckanapi import RemoteCKAN
 import pandas as pd
 
+
 ua = 'datapump/1.0'
 
-fileh = logging.FileHandler('./datapump.log', 'a')
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-fileh.setFormatter(formatter)
+logging.basicConfig(
+    filename='./datapump.log',
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    force=True,
+    level=logging.INFO
+)
 
-logger = logging.getLogger()
-logger.addHandler(fileh)
+# helper for logging to file and console
+def logecho(message, level='info', verbose=False):
+    message = level.upper() + ': ' + message
+    if level == 'error':
+        logging.error(message)
+        click.echo(Fore.RED + message, err=True) if verbose else False
+    elif level == 'warning':
+        logging.warning(message)
+        click.echo(Fore.YELLOW + message) if verbose else False
+    elif level == 'debug':
+        logging.debug(message)
+        click.echo(Fore.GREEN + message) if verbose else False
+    else:
+        logging.info(message)
+        click.echo(message) if verbose else False
+
 
 @click.command()
 @click.option('--inputdir',
               type=click.Path(exists=True),
-              default='./input/', 
+              default='./input/',
               show_default=True,
               help='The directory where the job files are located.')
 @click.option('--processeddir',
@@ -31,29 +51,30 @@ logger.addHandler(fileh)
               help='The directory where job that were not successfully '
               'processed are moved.')
 @click.option('--host',
-			  required=True,
-			  help='CKAN host.')
+              required=True,
+              help='CKAN host.')
 @click.option('--apikey',
               required=True,
               help='CKAN api key to use.')
 @click.option('--verbose',
               is_flag=True,
               help='Show more information while processing.')
+@click_config_file.configuration_option(config_file_name='datapump.ini')
 def datapump(inputdir, processeddir, problemsdir, host, apikey, verbose):
-    """Pumps data into CKAN."""
-    logger.info('Starting datapump...')
+    """Pumps data into CKAN using a simple directory-based queueing system."""
+    logging.info('Starting datapump...')
 
     try:
-        portal = RemoteCKAN(host, user_agent=ua)
+        portal = RemoteCKAN(host, apikey=apikey, user_agent=ua)
     except:
-        logger.error('Cannot connect to host %s' % host)
-        click.echo('Cannot connect to host %s' % host) if verbose else False
+        logecho('Cannot connect to host %s' %
+                host, level='error', verbose=verbose)
+        sys.exit()
     else:
-        click.echo('Connected to host %s' % host) if verbose else False
-        logger.info('Connected to host %s' % host)
+        logecho('Connected to host %s' % host, verbose=verbose)
     groups = portal.action.group_list()
 
-    logger.info('Ending datapump...')
+    logging.info('Ending datapump...')
 
 
 if __name__ == '__main__':
