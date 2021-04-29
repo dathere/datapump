7dab06928d05d1fb7c22b2fe37e47b05c77c5dce
Joel Natividad <1980690+jqnatividad@users.noreply.github.com>
2021-04-29 09:04:26 -0400

    Datapump v1 RC1

--- datapump.py
+++ datapump.py
@@ -7,53 +7,9 @@ import pandas as pd
 import json
 import os
 import glob
-
-# global variables
-gua = 'datapump/1.0'
-gloglevel = 'info'
-gverbose = False
-
-logging.basicConfig(
-    filename='./datapump.log',
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    force=True,
-    level=logging.INFO
-)
-glogger = logging.getLogger('')
-
-# helper for logging to file and console
-def logecho(message, level='info'):
-    message = level.upper() + ': ' + message
-    if level == 'error':
-        logging.error(message)
-        click.echo(Fore.RED + message, err=True) if gverbose else False
-    elif level == 'warning':
-        logging.warning(message)
-        click.echo(Fore.YELLOW + message) if gverbose else False
-    elif level == 'debug':
-        logging.debug(message)
-        click.echo(Fore.GREEN + message) if gverbose else False
-    else:
-        logging.info(message)
-        click.echo(message) if gverbose else False
-
-
-def readjob(job):
-    with open(job) as f:
-        try:
-            jobdefn = json.load(f)
-        except ValueError as e:
-            return False
-        else:
-            return jobdefn
-
-
-def runjob(job):
-    inputfiles = glob.glob(job['InputFile'])
-    logecho('%s file/s found for %s: ' % (len(inputfiles), job['InputFile']) +
-            ', '.join(inputfiles), level=gloglevel)
-    latestfile = max(inputfiles, key=os.path.getctime)
-    logecho('Latest file: %s' % latestfile, level=gloglevel)
+from datetime import datetime
+from time import perf_counter
+import shutil
 
 
 @click.command()
@@ -90,19 +46,238 @@ def runjob(job):
 def datapump(inputdir, processeddir, problemsdir, host, apikey, verbose,
              debug):
     """Pumps data into CKAN using a simple directory-based queueing system."""
-    logging.info('Starting datapump...')
+
+    ua = 'datapump/1.0'
+
+    logging.basicConfig(
+        filename='./datapump.log',
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        force=True,
+        level=logging.INFO
+    )
+    logger = logging.getLogger('')
+    org_list = []
+
+    # helper for logging to file and console
+    def logecho(message, level='info'):
+        if level == 'error':
+            logging.error(message)
+            click.echo(Fore.RED + level.upper() + ': ' + Fore.WHITE +
+                       message, err=True) if verbose else False
+        elif level == 'warning':
+            logging.warning(message)
+            click.echo(Fore.YELLOW + level.upper() + ': ' +
+                       Fore.WHITE + message) if verbose else False
+        elif level == 'debug':
+            logging.debug(message)
+            click.echo(Fore.GREEN + level.upper() + ': ' +
+                       Fore.WHITE + message) if debug else False
+        else:
+            logging.info(message)
+            click.echo(message)
+
+    def _get_col_dtype(col):
+        if col.dtype == "object":
+
+            try:
+                col_new = pd.to_datetime(col.dropna().unique())
+                return ['timestamp', 'datetime']
+            except:
+                return ["text", 'string']
+
+        elif col.dtype == 'float64':
+            return ['float', 'float64']
+        elif col.dtype == 'int64':
+            return ['int', 'int64']
+        elif col.dtype == 'datetime64[ns]':
+            return ['timestamp', 'datetime']
+        else:
+            return ['text', 'string']
+
+    # helper for reading job json file
+    def readjob(job):
+        with open(job) as f:
+            try:
+                jobdefn = json.load(f)
+            except ValueError as e:
+                return False
+            else:
+                return jobdefn
+
+    # helper for updating job json file
+    def updatejob(job, jobinfo):
+        with open(job, 'w') as f:
+            json.dump(jobinfo, f, indent=4)
+
+    # helper for run job
+    def runjob(job):
+        joberror = False
+        joberrordetails = ''
+        t1_startdt = datetime.now()
+        t1_start = perf_counter()
+
+        inputfiles = glob.glob(job['InputFile'])
+        logecho('  %s file/s found for %s: ' %
+                (len(inputfiles), job['InputFile']))
+
+        # process files, order by most recent
+        inputfiles.sort(key=os.path.getmtime, reverse=True)
+        for inputfile in inputfiles:
+            logindent = 3
+            logecho('    Processing: %s...' % inputfile)
+
+            def custom_date_parser(x): return datetime.strptime(
+                x, "%y/%m/%d %H:%M:%S")
+            df = pd.read_csv(inputfile, parse_dates=[
+                             'DateTime'], date_parser=custom_date_parser)
+
+            colname_list = df.columns.tolist()
+
+            coltype_list = []
+            for column in df:
+                coltype_list.append(_get_col_dtype(df[column]))
+
+            fields_dictlist = []
+            for i in range(0, len(colname_list)):
+                fields_dictlist.append({
+                    "id": colname_list[i],
+                    "type": coltype_list[i][0]
+                })
+                if coltype_list[i][0] == 'timestamp':
+                    df[colname_list[i]] = df[colname_list[i]].astype(str)
+
+            logecho('FIELDS_DICTLIST: %s' % fields_dictlist, level='debug')
+
+            data_dict = df.to_dict(orient='records')
+
+            # check if resource exists
+            # this works only when TargetResource is an existing
+            # resource id hash
+            try:
+                resource = portal.action.resource_show(
+                    id=job['TargetResource'])
+            except:
+                logecho('    Resource "%s" is not a resource id.' %
+                        job['TargetResource'])
+                resource = ''
+
+            if not resource:
+                # resource doesn't exist. Check if package exists
+                try:
+                    package = portal.action.package_show(
+                        id=job['TargetPackage'])
+                except:
+                    package = ''
+
+                if not package:
+                    # package doesn't exist. Create it
+                    # first, check if TargetOrg exist
+                    logecho('    Creating package "%s"...' %
+                            job['TargetPackage'])
+
+                    if not (job['TargetOrg'] in org_list):
+                        logecho('TargetOrg "%s" does not exist!' %
+                                job['TargetOrg'], level='error')
+                        sys.exit()
+
+                    try:
+                        package = portal.action.package_create(
+                            name=job['TargetPackage'],
+                            private=False,
+                            owner_org=job['TargetOrg']
+                        )
+                    except Exception as e:
+                        logecho('Cannot create package "%s"!' %
+                                job['TargetPackage'], level='error')
+                        joberror = True
+                        joberrordetails = e
+                    else:
+                        logecho('    Created package "%s"...' %
+                                job['TargetPackage'])
+                else:
+                    logecho('    Package "%s" found...' % job['TargetPackage'])
+
+                logecho('PACKAGE: %s\n\nFIELDS: %s' %
+                        (package, fields_dictlist), level='debug')
+                # logecho('RECORDS: %s\n' % data_dict, level='debug')
+
+                # now check if resource name already exists in package
+                resource_exists = False
+                resources = package['resources']
+                for resource in resources:
+                    if resource['name'] == job['TargetResource']:
+                        resource_exists = True
+                        break
+
+                if resource_exists:
+                    logecho('    "%s" exists in package "%s". Doing datastore_upsert...' % (
+                        job['TargetResource'], job['TargetPackage']))
+                    try:
+                        result = portal.action.datastore_upsert(
+                            resource_id=resource['id'],
+                            records=data_dict,
+                            method='upsert',
+                            calculate_record_count=True
+                        )
+                    except Exception as e:
+                        logecho('Upsert failed', level='error')
+                        joberror = True
+                        joberrordetails = e
+                    else:
+                        logecho('    Upsert successful!')
+                else:
+                    logecho('    "%s" does not exist in package "%s". Doing datastore_create...' % (
+                        job['TargetResource'], job['TargetPackage']))
+
+                    resource = {
+                        "package_id": package['id'],
+                        "format": "csv",
+                        "name": job['TargetResource']
+                    }
+                    try:
+                        resource = portal.action.datastore_create(
+                            resource=resource,
+                            fields=fields_dictlist,
+                            records=data_dict,
+                            primary_key=job['PrimaryKey'],
+                            indexes=job['PrimaryKey']
+                        )
+                    except Exception as e:
+                        logecho('Cannot create resource "%s"!' %
+                                job['TargetResource'], level='error')
+                        joberror = True
+                        joberrordetails = e
+                    else:
+                        logecho('    Created resource "%s"...' %
+                                job['TargetResource'])
+
+            logecho('RESOURCE: %s' % resource, level='debug')
+
+        t1_stop = perf_counter()
+        t1_stopdt = datetime.now()
+
+        job["Processed"] = len(df.index)
+        job["StartTime"] = t1_startdt.strftime('%Y-%m-%d %H:%M:%S')
+        job["EndTime"] = t1_stopdt.strftime('%Y-%m-%d %H:%M:%S')
+        job["Elapsed"] = t1_stop - t1_start
+        if joberror:
+            job["ReturnCode"] = joberrordetails.args
+            job["ReturnCodeMsg"] = joberrordetails.message
+        else:
+            job["ReturnCode"] = 0
+            job["ReturnCodeMsg"] = ''
+
+        return job
+
+    # datapump func main
+    logecho('Starting datapump...')
 
     if debug:
-        glogger.setLevel(logging.DEBUG)
-        gloglevel = 'debug'
-    else:
-        gloglevel = 'info'
-
-    gverbose = verbose
+        logger.setLevel(logging.DEBUG)
 
     # log into CKAN
     try:
-        portal = RemoteCKAN(host, apikey=apikey, user_agent=gua)
+        portal = RemoteCKAN(host, apikey=apikey, user_agent=ua)
     except:
         logecho('Cannot connect to host %s' %
                 host, level='error')
@@ -110,18 +285,31 @@ def datapump(inputdir, processeddir, problemsdir, host, apikey, verbose,
     else:
         logecho('Connected to host %s' % host)
 
+    org_list = portal.action.organization_list()
+
     # read jobs
     jobs = os.scandir(inputdir)
     for job in jobs:
         if (not job.name.startswith('.') and job.name.endswith('-job.json') and
                 job.is_file()):
-            logecho('Reading job - %s' % job)
+            logecho('  Reading job - %s' % job)
             jobdefn = readjob(job)
             if jobdefn:
-                logecho(json.dumps(jobdefn), level=gloglevel)
-                runjob(jobdefn)
+                logecho(json.dumps(jobdefn), level='debug')
+                jobinfo = runjob(jobdefn)
 
-    logging.info('Ending datapump...')
+            updatejob(job, jobinfo)
+
+            if jobinfo['ReturnCode'] == 0:
+                # job was successfully processed, move to processeddir
+                shutil.move(inputdir + '/' + job.name,
+                            processeddir + '/' + job.name)
+            else:
+                # job failed, move to problemsdir
+                shutil.move(inputdir + '/' + job.name,
+                            problemsdir + '/' + job.name)
+
+    logecho('Ending datapump...')
 
 
 if __name__ == '__main__':
